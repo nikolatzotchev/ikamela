98aaebb6f02339a833017eabe1cebeb316445aa3
petkosamoletko <ntzotchev@gmail.com>
2023-07-02 19:58:45 +0200

    very basic operations on integers

---
+++ bin/execution.ml
@@ -0,0 +1,52 @@
+(* Stack module, we need a custom one in order to implement also the other operations*)
+(* we do not have only pop and push*)
+module Stack = struct
+  type 'a t = 'a list
+
+  let empty = []
+  let is_empty stack = match stack with [] -> true | _ -> false
+  let push x stack = x :: stack
+
+  let pop stack =
+    match stack with [] -> failwith "Stack is empty" | x :: xs -> (x, xs)
+end
+
+(* Calculator function *)
+(* right now only basic operation on integers are supported*)
+(* and we do not look at the operation mode*)
+let calculate expression =
+  let stack = ref Stack.empty and operation_mode = ref 0 in
+
+  let apply_operator operator =
+    let operand2, stack' = Stack.pop !stack in
+    let operand1, stack'' = Stack.pop stack' in
+    match operator with
+    | '+' -> stack := Stack.push (operand1 + operand2) stack''
+    | '-' -> stack := Stack.push (operand1 - operand2) stack''
+    | '*' -> stack := Stack.push (operand1 * operand2) stack''
+    | '/' -> stack := Stack.push (operand1 / operand2) stack''
+    | _ -> failwith "Invalid operator"
+  in
+
+  let process_token token =
+    match token with
+    | '+' | '-' | '*' | '/' -> operation_mode := 0; apply_operator token
+    | '0' .. '9' ->
+        (match !operation_mode with
+        | 0 -> stack := Stack.push (int_of_char token - int_of_char '0') !stack;  operation_mode := -1
+        | _ when !operation_mode < 0 ->
+            let number, stack' = Stack.pop !stack in
+              stack := Stack.push ((int_of_char token - int_of_char '0') + number * 10) stack'
+        | _  -> failwith "test")
+    | ' ' -> operation_mode := 0
+    | _ -> failwith "unsuported"
+      
+  in
+
+  let tokens = List.of_seq (String.to_seq expression) in
+
+  List.iter process_token tokens;
+
+  let result, _ = Stack.pop !stack in
+  (*this is the result of the whole operation*)
+  result
--- bin/registers.ml
+++ bin/registers.ml
@@ -1,10 +1,9 @@
 module RegistersMap = Map.Make (String)
 
-let init_regs m = RegistersMap.add "a" "2+2" m
+let init_regs m = RegistersMap.add "a" "15 2 3 4+*-" m
 
 let print_registers m =
   let print_reg key value =
     print_endline ("reg: " ^ key ^ " value: " ^ value)
   in
   RegistersMap.iter print_reg m
-
