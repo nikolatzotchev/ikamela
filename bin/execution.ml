f4038ef5de0219dde002604e3f1ad71476190362
Yleisnero <fischi12325@gmail.com>
2023-07-07 11:44:32 +0200

    Move epsilon inside the compare function

--- bin/execution.ml
+++ bin/execution.ml
@@ -30,9 +30,8 @@ let char_to_stack_int char =
 (* Converts a char into an integer*)
 let char_to_int char = int_of_char char - int_of_char '0'
 
-
-let epsilon = 0.0001
 let compare_float v u =
+  let epsilon = 0.0001 in
   let diff = abs_float (v -. u) in
   if abs_float v > 1.0 || abs_float u > 1.0 then
     if diff <= epsilon *. max (abs_float v) (abs_float u) then 0 else 1
