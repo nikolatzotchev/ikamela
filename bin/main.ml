042923626f66c59e3ecffdf3aac2b94dc32e96cd
Lorenz Kofler <lorenz.kofler@gmail.com>
2023-08-26 11:57:39 +0200

    Add brackets to string, since they need it :)
    Signed-off-by: Lorenz Kofler <lorenz.kofler@gmail.com>

--- bin/main.ml
+++ bin/main.ml
@@ -10,15 +10,15 @@ let rec read_input () =
 
 let () =
   Registers.set_value "a" (String { value = "Welcome! Let's calculate stuff! Woohoo!" });
-  Registers.set_value "b" (Integer { value = 1 });
+  Registers.set_value "b" (Integer { value = 21 });
   Registers.set_value "c" (Float { value = 3.14 });
-  Registers.set_value "d" (String { value = "15 2 3 4+*-" });
-  Registers.set_value "e" (String { value = "15 2+" });
-  Registers.set_value "e" (String { value = "15 2+" });
-  Registers.set_value "f" (String { value = "15" });
-  Registers.set_value "g" (String { value = "15 1*" });
-  Registers.set_value "h" (String { value = "15 2*" });
-  Registers.set_value "i" (String { value = "15 3*" });
+  Registers.set_value "d" (String { value = "(15 2 3 4+*-)" });
+  Registers.set_value "e" (String { value = "(15 2+)" });
+  Registers.set_value "f" (String { value = "(15)" });
+  Registers.set_value "g" (String { value = "(15 1*)" });
+  Registers.set_value "h" (String { value = "(15 2*)" });
+  Registers.set_value "i" (String { value = "(15 3*)" });
+  Registers.set_value "j" (String { value = "(9 2+)" });
   (*Registers.set_value "b"  "15 2 3 4+*-";*)
   (*Registers.set_value "c" "2((2 2*)@*)@";*)
   (*Registers.set_value "d" "(2*)\\2";*)
