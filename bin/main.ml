2415a4dea8e3de5abfc3b59ade01c0b3d902b820
petkosamoletko <ntzotchev@gmail.com>
2023-09-15 18:15:21 +0200

    now median is ready no cap

--- bin/main.ml
+++ bin/main.ml
@@ -13,23 +13,24 @@ let rec read_input () =
 let () =
   set_value "a" (String { value = "(Welcome! Let's calculate stuff! Woohoo!)" });
   (* calculate median *)
+  set_value "m" (String { value = "((#1+! #! > (#1+! #$) (#! #1-$) c@ 4! 4$ 4! 1- 4$  m@) () (4!_1+$@)@)" });
+  set_value "t" (String { value = "((1-#2-m@#$1$t@)()(4!_1+$@)@)" });
   set_value "i" (String { value = "((#1+! #! > (#! #1-$) (#1+! #$) c@ 4! 4$ 4! 1- 4$  i@) () (4!_1+$@)@)" });
-  set_value "g" (String { value = "((1-#2-i@#$1$g@)(1$y@)(4!_1+$@)@)" });
-  set_value "h" (String { value = "(2! 2% (2/g@)() c@ )" });
+  set_value "g" (String { value = "((1-#2-i@#$1$g@)()(4!_1+$@)@)" });
+  set_value "h" (String { value = "(2! 2% (2/g@1$y@)(2/1-g@1$#m@1$+2/y@) c@ )" });
+  (* set_value "h" (String { value = "(2! 2% (2/g@1$y@)(2/1-g@1$#2-t@) c@ )" }); *)
+  (* this is the mean if you want to test use j@ in the 'console' and then input the array like this (1.5 1.8 1.0 1.2 2.5) *)
   set_value "j" (String { value = "('@ # h@)" });
   
   (* help function for the average*)
   set_value "n" (String { value = "(('+3!3$1-3!3$n@)(2$3!3$/)(5!_1+$@)@)" });
   (* this calculates the average (arithmetic mean) the main code is in register n *)
   set_value "q" (String { value = "(' 2! 0 n@)"});
-  
+
+  (* find the median*)
+  set_value "w" (String { value = "('@)"}); 
   set_value "x" (String { value = "(9 7 8 #! (#-1)@! > (0) (1) (4!4$_1+$@)@)"});
-  set_value "n" (String { value = "(no)" });
-  set_value "b" (String { value = "(3! 3! >)" }); (*is the first bigger than the second*)
   set_value "c" (String { value = "(4!4$_1+$@)"});
-  set_value "d" (String { value = "(9 7 8)"});
-  set_value "d" (String { value = "(9 8 7)"});
-  set_value "x" (String { value = "(9 8 7 (3! 3! >)@ (1$) (2$) c@)"});
 
   set_value "z" (String { value = "((3! 3! >)@ (1$) (2$) c@)"}); (*compare  2 value and remove lower one*)
   set_value "x" (String { value = "((3! 3! >)@ () (3! 3$) c@)"}); (*order 2 values*)
