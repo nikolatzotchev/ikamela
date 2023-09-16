5c018ee45c8c100bf1f74a6349d378e45c96a130
petkosamoletko <ntzotchev@gmail.com>
2023-09-16 11:51:51 +0200

    refactored mean

--- bin/main.ml
+++ bin/main.ml
@@ -18,18 +18,21 @@ let () =
   set_value "i" (String { value = "((#1+! #! > (#! #1-$) (#1+! #$) c@ 4! 4$ 4! 1- 4$  i@) () (4!_1+$@)@)" });
   set_value "g" (String { value = "((1-#2-i@#$1$g@)()(4!_1+$@)@)" });
   set_value "h" (String { value = "(2! 2% (2/g@1$y@)(2/1-g@1$#m@1$+2/y@) c@ )" });
-  (* this is the mean if you want to test use j@ in the 'console' and then input the array like this (1.5 1.8 1.0 1.2 2.5) *)
+  (* this is the median if you want to test use j@ in the 'console' and then input the array like this (1.5 1.8 1.0 1.2 2.5) *)
   set_value "j" (String { value = "('@ # h@)" });
   
   (* help function for the average*)
-  set_value "n" (String { value = "(('+3!3$1-3!3$n@)(2$3!3$/)(5!_1+$@)@)" });
   (* this calculates the average (arithmetic mean) the main code is in register n *)
-  set_value "q" (String { value = "(' 2! 0 n@)"});
+  set_value "v" (String { value = "(# 1 > (#$v@) () c@)"}); 
+  set_value "n" (String { value = "((#3!-2+!4!4$+3!3$1-n@)(1$#1-/v@)(4!_1+$@)@)"}); 
+  (* this is the mean *)
+  set_value "q" (String { value = "('@ 0 #1- n@)"});
 
   (* find the variance*)
   set_value "e" (String { value = "((1-e@#$)(1$)(4!_1+$@)@)"}); 
   set_value "o" (String { value = "((#3!-2+!5!-2!*4!4$+3!3$1-o@)(1$2$#2-/#1-e@)(4!_1+$@)@)"}); 
   set_value "p" (String { value = "((#3!-2+!4!4$+3!3$1-p@)(1$#1-/0#2-o@)(4!_1+$@)@)"}); 
+  (* this is the register for variance*)
   set_value "w" (String { value = "('@ 0 #1- p@)"}); 
 
   set_value "x" (String { value = "(9 7 8 #! (#-1)@! > (0) (1) (4!4$_1+$@)@)"});
